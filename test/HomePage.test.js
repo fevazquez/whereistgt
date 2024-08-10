1de37d74466adf1d927039f6067f2c20f8538060
fevazquez <44679685+fevazquez@users.noreply.github.com>
2024-08-10 22:29:12 +0000

    Build: (10cee01) Merge pull request #3 from fevazquez/dev
    Updates the github workflow config

--- test/HomePage.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-
-import { HomePage } from '../src/components';
-
-test('Renders drink check', () => {
-  render(<HomePage />);
-  expect(screen.getByText("drink check...")).toBeInTheDocument();
-});

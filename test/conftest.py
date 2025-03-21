5e1ec73f39a457ea4248113142e4548c778714c3
Fernando Vazquez <f4vazquez@gmail.com>
2025-03-20 23:20:57 -0700

    Update python tests config

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 def pytest_addoption(parser):
     parser.addoption("--url", action="store", default="http://localhost", help="url")
@@ -14,8 +16,9 @@ def browser():
     options.add_argument('--single-process')
     options.add_argument('--disable-dev-shm-usage')
 
-    browser = webdriver.Chrome(options=options)
-    return browser
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    yield driver  # This will allow the browser instance to persist for multiple tests
+    driver.quit()  # Ensure the browser is closed after all tests 
 
 @pytest.fixture(scope="module")
 def url(request):
